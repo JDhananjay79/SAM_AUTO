70b5573081a2d2b75c4af7996855a8dd67499ada
Suraj Bhalerao <surajbhalerao2024@gmail.com>
2026-02-10 18:18:26 +0530

    update randon gen class

--- src/main/java/com/aepl/sam/utils/RandomGeneratorUtils.java
+++ src/main/java/com/aepl/sam/utils/RandomGeneratorUtils.java
@@ -3,85 +3,121 @@ package com.aepl.sam.utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.security.SecureRandom;
+
 public class RandomGeneratorUtils {
-	private static final Logger logger = LogManager.getLogger(RandomGeneratorUtils.class);
 
-	public String generateRandomString(int length) {
-		String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-		StringBuilder result = new StringBuilder(length);
+    private static final Logger logger = LogManager.getLogger(RandomGeneratorUtils.class);
+    private static final SecureRandom RANDOM = new SecureRandom();
 
-		for (int i = 0; i < length; i++) {
-			int index = (int) (Math.random() * characters.length());
-			result.append(characters.charAt(index));
-		}
+    private static final String ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
 
-		String randomString = result.toString();
-		logger.info("Generated random string of length {}: {}", length, randomString);
+    public String generateRandomString(int length) { 
+        if (length <= 0) {
+            logger.warn("Requested random string with non-positive length: {}", length);
+            return "";
+        }
 
-		return randomString;
-	}
+        StringBuilder result = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            result.append(ALPHABETS.charAt(RANDOM.nextInt(ALPHABETS.length())));
+        }
 
-	public String generateRandomNumber(int length) {
-		if (length <= 0) {
-			logger.warn("Requested random number with non-positive length: {}", length);
-			return "";
-		}
+        String randomString = result.toString();
+        logger.debug("Generated random string of length {}", length); // avoid logging value
+        return randomString;
+    }
 
-		StringBuilder result = new StringBuilder(length);
-		int[] allowedFirstDigits = { 7, 8, 9 };
-		int firstDigit = allowedFirstDigits[(int) (Math.random() * allowedFirstDigits.length)];
-		result.append(firstDigit);
+    public String generateRandomNumber(int length) {
+        if (length <= 0) {
+            logger.warn("Requested random number with non-positive length: {}", length);
+            return "";
+        }
 
-		for (int i = 1; i < length; i++) {
-			int digit = (int) (Math.random() * 10);
-			result.append(digit);
-		}
+        StringBuilder result = new StringBuilder(length);
 
-		String randomNumber = result.toString();
-		logger.info("Generated random number of length {}: {}", length, randomNumber);
+        // First digit constraint (7, 8, or 9)
+        int[] allowedFirstDigits = {7, 8, 9};
+        result.append(allowedFirstDigits[RANDOM.nextInt(allowedFirstDigits.length)]);
 
-		return randomNumber;
-	}
+        for (int i = 1; i < length; i++) {
+            result.append(DIGITS.charAt(RANDOM.nextInt(10)));
+        }
 
-	public String generateRandomEmail() {
-		String prefix = generateRandomString(7);
-		String domain = "gmail.com";
-		String email = prefix + "@" + domain;
+        String randomNumber = result.toString();
+        logger.debug("Generated random number of length {}", length);
+        return randomNumber;
+    }
 
-		logger.info("Generated random email: {}", email);
-		return email;
-	}
+    public String generateRandomEmail() {
+        String prefix = generateRandomString(7).toLowerCase();
+        String domain = "gmail.com";
+        String email = prefix + "@" + domain;
 
-	public String generateRandomUIN() {
-		StringBuilder uin = new StringBuilder("ACON4SA");
-		for (int i = 0; i < 12; i++) {
-			int digit = (int) (Math.random() * 10);
-			uin.append(digit);
-		}
-		String finalUIN = uin.toString();
-		logger.info("Generated random UIN: {}", finalUIN);
-		return finalUIN;
-	}
+        logger.debug("Generated random email");
+        return email;
+    }
 
-	public String generateRandomIMEI() {
-		StringBuilder imei = new StringBuilder();
-		for (int i = 0; i < 15; i++) {
-			int digit = (int) (Math.random() * 10);
-			imei.append(digit);
-		}
-		String finalIMEI = imei.toString();
-		logger.info("Generated random IMEI: {}", finalIMEI);
-		return finalIMEI;
-	}
+    public String generateRandomUIN() {
+        StringBuilder uin = new StringBuilder("ACON4SA");
+        for (int i = 0; i < 12; i++) {
+            uin.append(RANDOM.nextInt(10));
+        }
 
-	public String generateRandomICCID() {
-		StringBuilder iccid = new StringBuilder();
-		for (int i = 0; i < 20; i++) {
-			int digit = (int) (Math.random() * 10);
-			iccid.append(digit);
-		}
-		String finalICCID = iccid.toString();
-		logger.info("Generated random ICCID: {}", finalICCID);
-		return finalICCID;
-	}
+        String finalUIN = uin.toString();
+        logger.debug("Generated random UIN");
+        return finalUIN;
+    }
+
+    public String generateRandomIMEI() {
+        // Generate first 14 digits
+        StringBuilder imei = new StringBuilder(14);
+        for (int i = 0; i < 14; i++) {
+            imei.append(RANDOM.nextInt(10));
+        }
+
+        // Calculate Luhn check digit
+        int checkDigit = calculateLuhnCheckDigit(imei.toString());
+        imei.append(checkDigit);
+
+        String finalIMEI = imei.toString();
+        logger.debug("Generated random IMEI");
+        return finalIMEI;
+    }
+
+    public String generateRandomICCID() {
+        // ICCID typically starts with telecom identifier like 89
+        StringBuilder iccid = new StringBuilder("89");
+
+        while (iccid.length() < 19) { // 19 digits before Luhn
+            iccid.append(RANDOM.nextInt(10));
+        }
+
+        int checkDigit = calculateLuhnCheckDigit(iccid.toString());
+        iccid.append(checkDigit);
+
+        String finalICCID = iccid.toString();
+        logger.debug("Generated random ICCID");
+        return finalICCID;
+    }
+
+    // ------------------ Helper ------------------
+
+    private int calculateLuhnCheckDigit(String number) {
+        int sum = 0;
+        boolean alternate = true;
+
+        for (int i = number.length() - 1; i >= 0; i--) {
+            int n = number.charAt(i) - '0';
+            if (alternate) {
+                n *= 2;
+                if (n > 9) n -= 9;
+            }
+            sum += n;
+            alternate = !alternate;
+        }
+
+        return (10 - (sum % 10)) % 10;
+    }
 }
